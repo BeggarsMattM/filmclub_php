659e78d93ea8b7e09cecd690a2bd7edfbf3cb767
Matthew Marcus <matthewmarcus@beggars.com>
2013-07-31 14:29:36 +0100

    Finetuning user add process

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
+App::uses('AuthComponent', 'Controller/Component');
 /**
  * User Model
  *
@@ -14,6 +15,28 @@ class User extends AppModel {
  */
 	public $displayField = 'username';
 
+
+    public $validate = array(
+        'username' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A username is required'
+            )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A password is required'
+            )
+        ),
+        'role' => array(
+            'valid' => array(
+                'rule' => array('inList', array('admin', 'author')),
+                'message' => 'Please enter a valid role',
+                'allowEmpty' => false
+            )
+        )
+    );
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
@@ -37,5 +60,12 @@ class User extends AppModel {
 			'counterQuery' => ''
 		)
 	);
+
+		public function beforeSave($options = array()) {
+			if (isset($this->data[$this->alias]['password'])) {
+				$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+			}
+			return true;
+		}
 
 }
